b01c316d090e0bf69c4dd54a67893e5817cced61
profelizabethduane <36997590+profelizabethduane@users.noreply.github.com>
2025-06-10 10:35:24 -0300

    Add files via upload

---
+++ 2025_06_10/fila_inicial.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+class Fila
+{
+private:
+	int *dados_;
+	int frente_;
+	int tras_;
+	int tamanho_atual_; // quantos elementos foram inseridos
+	int capacidade_;	// tamanho total da fila
+
+public:
+	// operações
+
+	// inicializa uma fila vazia com a capacidade máxima igual à
+	// capacidade
+	// inicializa os atributos da classe
+	Fila(int capacidade)
+	{
+	}
+	// retorna verdadeiro se a fila estiver vazia
+	// retorna falso, caso contrário
+	bool EstaVazia()
+	{
+	}
+	// retorna verdadeiro se a fila estiver cheia
+	// retorna falso, caso contrário
+	bool EstaCheia()
+	{
+	}
+
+	void Enfileira(int valor)
+	{
+		// verifica se a fila está cheia
+
+		// insere valor na posição de trás da file
+	}
+
+	// retorna o elemento da frente da fila
+	int Frente()
+	{
+	}
+	// retira o elemento da frente da fila
+	void Desenfileira()
+	{
+	}
+};
+
+int main()
+{
+
+	// solicite ao usuário o tamanho máximo da fila.
+	// crie uma fila vazia
+	// faça um loop para que o usuário escolha entre as opçoes:
+	//  1. incluir um número na fila
+	//  2. remover um número da fila
+	//  3. listar todos os números da fila na ordem de inserção
+	//  4. sair
+
+	// o item 3 deve ser implementado fora da classe.
+	return 0;
+}
